796b613196cd4291acb4c5a7979e8e89689a8776
nai10 <63729571+nai10@users.noreply.github.com>
2021-01-13 23:10:37 +0900

    completed step 31

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し初期化
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divへのli追加
+  div.appendChild(li);
+
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+
+  console.log(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
